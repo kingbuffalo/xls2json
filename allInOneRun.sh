927f976c6a63ef405376b0b827efbb2affe9e4b7
kingbuffalo <kinghexi@126.com>
2021-06-12 23:18:56 +0800

    a

--- allInOneRun.sh
+++ allInOneRun.sh
@@ -1,2 +1,3 @@
+cp /home/cds/work/stkingxls/*.xlsx ./examplePoj/excel/
 sh runjson2lua.sh
 sh cpEnum.sh
--- cpEnum.sh
+++ cpEnum.sh
@@ -1,2 +1,3 @@
 cp examplePoj/goenum/cfgEnumConst.lua /home/cds/work/skgamesvr/src/services/misc/
 cp examplePoj/lua/*.lua /home/cds/work/skgamesvr/cfg/cfgorig/
+cp examplePoj/goenum/cfgEnumConst.lua /home/cds/work/skgamesvr/cfg/
